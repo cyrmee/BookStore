985ce8bcf478e5a924b98d2f2d6100c2ba8a90f7
Mehretab Ayalew <cyrmee@gmail.com>
2023-05-30 14:41:55 +0300

    removed baseDto

--- src/Application/DTOs/BaseDto.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Application.DTOs;
-
-public record BaseDto(Guid Id, [DataType(DataType.Date)] DateTime CreatedDate,
-    [DataType(DataType.Date)] DateTime UpdatedDate, string UpdatedBy, string AddedBy, bool IsDeleted);
